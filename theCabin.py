40b7d65466fffd2f7f181ccb9dca9dba484f0594
ParetoYXE <35578020+ParetoYXE@users.noreply.github.com>
2019-12-21 23:49:19 -0600

    Moose, Augs and Fire
    Moose now properly roam the landscape and produce food for your consumption. Augs have been added, currently in steep number, that hunt you down and drain your energy upon contact. You can also now activate 'attacks' by pressing 'A'. You throw Molotov Cocktails which currently deal no damage.

--- theCabin.py
+++ theCabin.py
@@ -3,12 +3,14 @@ pygame.init()
 width=800
 height=800
 background = [255,255,255]
-stats = [{'text':"Heat:",'X':661,'Y':80,'value':0},{'text':"Wood:",'X':664,'Y':120,'value':0},{'text':"Energy:",'X':663,'Y':160,'value':30},{'text':"Moves:",'X':663,'Y':190,'value':0}]
-entities = [{'name':'player','hp':3,'X':500,'Y':300},{'name':'cabin','hp':500,'X':width/2,'Y':height/2,"width":100,"height":100}]
+stats = [{'text':"Heat:",'X':661,'Y':80,'value':0},{'text':"Wood:",'X':664,'Y':120,'value':0},{'text':"Energy:",'X':663,'Y':160,'value':30},{'text':"Moves:",'X':663,'Y':190,'value':0},{'text':"Food:",'X':663,'Y':220,'value':0}]
+entities = [{'name':'player','hp':3,'X':500,'Y':300},{'name':'cabin','hp':500,'X':width/2,'Y':height/2,"width":100,"height":100},{'name':'augs','hp':5,'X':random.randint(0,width),'Y':random.randint(0,height),"width":10,"height":5}]
 for i in range(random.randint(10,30)):
 	entities.append({'name':'tree','hp':5,'X':random.randint(0,width),'Y':random.randint(0,height),"width":50,"height":90})
 for j in range(random.randint(1,4)):
 	entities.append({'name':'moose','hp':3,'X':random.randint(0,width),'Y':random.randint(0,height),"width":50,"height":90})
+for k in range(random.randint(1,50)):
+	entities.append({'name':'augs','hp':1,'X':random.randint(0,width),'Y':random.randint(0,height),"width":40,"height":40})
 screen = pygame.display.set_mode( [width, height ] )
 screen.fill(background)
 pygame.display.set_caption('100LOC')
@@ -43,9 +45,18 @@ def collisionDetection():
 					if(stats[0]['value'] > 0):
 						stats[2]['value']+= random.randint(3,7)
 						stats[0]['value']-=1
+					if(stats[4]['value'] > 0):
+						stats[2]['value']+= 1
+						stats[4]['value']-=1
 				if(entities[i]['name'] == "tree" and entities[i]['hp']>0):
 					stats[1]['value']+=1
 					entities[i]['hp']-=1
+				if(entities[i]['name'] == "aaugs" and entities[i]['hp']>0):
+					stats[2]['value']-=1
+				if(entities[i]['name'] == "moose" and entities[i]['hp']>0):
+					hit =  random.randint(0,10)
+					stats[4]['value'] += hit
+					entities[i]['hp'] -= hit
 def getInput(pos):
 	x,y = pos
 	if(entities[0]['X'] < x):
@@ -56,11 +67,21 @@ def getInput(pos):
 		entities[0]['Y'] +=20
 	if(entities[0]['Y'] > y):
 		entities[0]['Y'] -=20
+def npcAI():
+	for i in entities:
+		if i['name'] == 'moose':
+			i['X'] += random.randint(-5,5)
+			i['Y'] += random.randint(-5,5)
+		if i['name'] == 'augs':
+			i['X'] += (entities[0]['X'] - i['X']) / 10
+			i['Y'] += (entities[0]['Y'] - i['Y']) / 10
+
 drawEntities()
 drawText()
 #only call this if necessary. Not in a loop. Reduce Big O()
 pygame.display.update()
 running = True
+attack = False
 while (running):
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -72,7 +93,11 @@ while (running):
 					stats[2]['value'] -= 1
 					collisionDetection()
 					getInput(event.pos)
+				if(attack):
+					x,y = event.pos
+					entities.append({'name':'fireAttack','hp':3,'X':x,'Y':y,'width':10, 'height':10})
 				screen.fill(background)
+				npcAI()
 				drawEntities()
 				drawText()
 				if(stats[2]['value']<=0):
@@ -82,11 +107,18 @@ while (running):
 				pygame.display.update()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_r:
-				stats = [{'text':"Heat:",'X':661,'Y':80,'value':0},{'text':"Wood:",'X':664,'Y':120,'value':0},{'text':"Energy:",'X':663,'Y':160,'value':30},{'text':"Moves:",'X':663,'Y':190,'value':0}]
-				entities = [{'name':'player','hp':3,'X':500,'Y':300},{'name':'cabin','hp':500,'X':width/2,'Y':height/2,"width":100,"height":100}]
+				stats = [{'text':"Heat:",'X':661,'Y':80,'value':0},{'text':"Wood:",'X':664,'Y':120,'value':0},{'text':"Energy:",'X':663,'Y':160,'value':30},{'text':"Moves:",'X':663,'Y':190,'value':0},{'text':"Food:",'X':663,'Y':220,'value':0}]
+				entities = [{'name':'player','hp':3,'X':500,'Y':300},{'name':'cabin','hp':500,'X':width/2,'Y':height/2,"width":100,"height":100},{'name':'augs','hp':5,'X':random.randint(0,width),'Y':random.randint(0,height),"width":10,"height":5}]
 				for i in range(random.randint(4,12)):
 					entities.append({'name':'tree','hp':5,'X':random.randint(0,width),'Y':random.randint(0,height),"width":50,"height":90})
+				for j in range(random.randint(1,4)):
+					entities.append({'name':'moose','hp':3,'X':random.randint(0,width),'Y':random.randint(0,height),"width":50,"height":90})
+				for k in range(random.randint(1,50)):
+					entities.append({'name':'augs','hp':1,'X':random.randint(0,width),'Y':random.randint(0,height),"width":40,"height":40})
 				screen.fill(background)
 				drawEntities()
 				drawText()
 				pygame.display.update()
+			if event.key == pygame.K_a:
+				attack = not attack
+				print(attack)
